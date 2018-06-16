a33fe78a2894a49ce361f75e32045b7a8bf8b0e1
Arjun Thekkumadathil <arjun@Arjuns-MacBook-Pro.local>
2018-06-16 14:33:59 +0100

    cleaned up UI, added flag to change the bg color

--- src/popper_gui.py
+++ src/popper_gui.py
@@ -3,31 +3,30 @@ import sys
 import argparse
 from tkinter import Tk, Label, Button
 
+WINDOW_TITLE = 'Popper Message !!'
 class PopperGui:
-    def __init__(self, master, sender, message):
+    def __init__(self, master, args):
         self.master = master
-        master.title("Popper Message")
+        master.title(WINDOW_TITLE)
 
-        self.label = Label(master, text="From: {0}".format(sender))
+        self.label = Label(master, bg=args.bgcolor, fg='white', text="From: {0}".format(args.sender))
         self.label.pack()
 
-        self.message = Label(master, text="From: {0}".format(message))
+        self.message = Label(master, bg=args.bgcolor, fg='white', text="Message: {0}".format(args.message))
         self.message.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-
-    def greet(self):
-        print("Greetings!")
 
 def main(args):
     root = Tk()
-    my_gui = PopperGui(root, args.sender, args.message)
+    root.minsize(width=500, height=200)
+    root.configure(background=args.bgcolor)
+    message_gui = PopperGui(root, args)
     root.mainloop()
 
 if __name__ == '__main__':
     arguments = argparse.ArgumentParser("Gui to display the message for chatter")
     arguments.add_argument('--sender', type=str, help='Name / user id of the sender')
     arguments.add_argument('--message', type=str, help='Message from the sender')
+    arguments.add_argument('--bgcolor', type=str, default='grey', help='Message display window background color')
     args = arguments.parse_args()
     main(args)
